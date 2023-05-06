df4d9f26dc06d7e8799b080d71c1dc0e24d92388
Nameer Sharma <nameersharma2003@gmail.com>
2023-05-06 09:45:46 -0700

    First Commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,55 @@
+import React, { useRef, useState } from "react";
+import { motion } from "framer-motion";
+
+import {FaInstagram, FaLinkedin, FaTwitter, FaGithub} from 'react-icons/fa';
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
+import { fadeIn, textVariant } from "../utils/motion";
+import './contact.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+const Contact = () => {
+  return (
+    <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10`}>
+      <motion.div variants={slideIn("left", 'tween', 0.2, 1)} className="flex-[0.50] bg-black-100 p-8 rounded-2xl" >
+        <p className={styles.sectionSubText}>Contact</p>
+        <h3 className={styles.sectionHeadText}>Let's Connect!</h3>
+        <div className="wrapper">
+          <div className="button">
+            <div className="icon">
+              <i className="fab fa-linkedin"></i>
+            </div>
+            <span>LinkedIn</span>
+          </div>
+          <div className="button">
+            <div className="icon">
+              <i className="fab fa-google"></i>
+            </div>
+            <span>Gmail</span>
+          </div>
+          <div className="button">
+            <div className="icon instagram">
+              <i className="fab fa-instagram"></i>
+            </div>
+            <span>Instagram</span>
+          </div>
+          <div className="button">
+            <div className="icon github">
+              <i className="fab fa-github"></i>
+            </div>
+            <span>Github</span>
+          </div>
+        </div>
+      </motion.div>
+      {/* <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'>
+        <EarthCanvas />
+      </motion.div> */}
+    </div>
+  );
+};
+
+export default SectionWrapper(Contact, "contact");
---
+++ src/components/Progress.jsx
@@ -0,0 +1,49 @@
+import React, {useEffect, useState } from 'react';
+
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { fadeIn, textVariant } from "../utils/motion";
+import { slideIn } from "../utils/motion";
+import './progress.css';
+
+import { progress } from "../constants";
+
+import { SectionWrapper } from "../hoc";
+
+const ProgressCard = ({name, description, progressBar}) => {
+    return (
+        <motion.div variants={slideIn("left", 'tween', 0.2, 1)} className='container bg-tertiary p-4 rounded-5xl sm:w-[360px] w-full'>
+            <div className='skill-box'>
+                <span className='title'>{name}</span>
+                <div className='skill-bar'>
+                    <span className='skill-per' style={{width: `${progressBar}%`}}>
+                        <span className='tooltip'>{progressBar}</span>
+                    </span>
+                </div>
+            </div>
+        </motion.div>
+    )
+}
+
+const Progress = () => {
+    return (
+        <>
+            <motion.div variants={textVariant()}>
+                <p className={styles.sectionSubText}>Progress</p>
+                <h2 className={styles.sectionHeadText}>What I am Working On</h2>
+            </motion.div>
+
+            <div className="mt-20 flex flex-wrap gap-7">
+                {progress.map((progress, index) => (
+                    <ProgressCard 
+                        index={index}
+                        {...progress} 
+                    />
+                ))}
+                </div>
+        </>
+    )
+}
+
+export default SectionWrapper(Progress,"");
