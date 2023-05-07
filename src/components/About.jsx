6f270e75a7058b518150f87b8be975e6a78b1103
Nameer Sharma <nameersharma2003@gmail.com>
2023-05-07 09:35:54 -0700

    3rd

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -34,7 +34,7 @@ const About = () => {
     <>
       <motion.div variants ={textVariant()}>
         <p className={styles.sectionSubText}>Intro</p>
-        <h2 className={styles.sectionHeadText}>Overview</h2>
+        <h2 className={styles.sectionHeadText}>Overview-</h2>
       </motion.div>
       <motion.p variants={fadeIn("", "", 0.1, 1)} className='mt-4 text-secondary text-[17px] mx-w-3xl leading-[30px]'> 
         I am a Software Developer
